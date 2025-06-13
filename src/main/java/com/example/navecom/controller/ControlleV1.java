7b4a51a767f32be3f3253919016412adc61d193b
mauro.baldi <baldi_mm@libero.it>
2025-06-13 16:03:25 +0200

    send rotta id2

--- src/main/java/com/example/navecom/controller/ControlleV1.java
+++
@@ -1,4 +0,0 @@
-package com.example.navecom.controller;
-
-public class ControlleV1 {
-}
---
+++ src/main/java/com/example/navecom/controller/ControllerV1.java
@@ -0,0 +1,27 @@
+package com.example.navecom.controller;
+
+
+import com.example.navecom.service.MissionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class ControlleV1 {
+    private final MissionService missionService;
+
+
+
+    @GetMapping("/checkRotta/{missionId}")
+    public ResponseEntity<String> checkRotta(@RequestParam long missionId) {
+        missionService.sendRotta(missionId);
+        return ResponseEntity.ok("Rotta inviata: " + missionId);
+
+    }
+}
