7c55e8b7f69b635dee8d1b64d0b7072e2937f697
Mauro Baldi <mauro.baldi@esterni.elis.org>
2025-06-13 12:18:22 +0200

    first push

---
+++ src/main/java/com/example/navecom/NaveComApplication.java
@@ -0,0 +1,13 @@
+package com.example.navecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaveComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaveComApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/navecom/NaveComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.navecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaveComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
